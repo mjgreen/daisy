217d3c3112ffead451482e5a97966fa3230aeeea
mgreen1718 <mgreen1718@gmail.com>
2023-09-12 11:52:25 +0100

    add vignette url

--- daisy1.R
+++ daisy1.R
@@ -1,1 +1,12 @@
 library("Superpower")
+# https://cran.r-project.org/web/packages/Superpower/index.html
+# Vignettes:
+
+# compromise_power       Introduction to Justifying Alpha Levels
+# intro_to_superpower    Introduction to Superpower 
+# more_anova_designs     More ANOVA designs
+# emmeans_power          Power calculations for `emmeans` analyses
+# ANCOVAs                Power Calculations for ANCOVA 
+
+vignette("intro_to_superpower")
+# https://cran.r-project.org/web/packages/Superpower/vignettes/intro_to_superpower.html
